2547276849c8c330614ba06224d6f2f046da6ac0
Webbutveckling <webbutveckling@MacBook-Pro-som-tillhor-Elias.local>
2024-09-09 10:53:33 +0200

    added some js to the site

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const myDiv = document.getElementbyId("myDiv");
+const myDiv = document.getElementById("container");
+
+myDiv.addEventListener("mouseover", function() {
+    myDiv.style.backgroundColor = "grey";
+});
+
+myDiv.addEventListener("mouseout", function() {
+    myDiv.style.backgroundColor = "brown";
+});
